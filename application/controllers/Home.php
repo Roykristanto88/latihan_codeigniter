036eb50fa04d45b5c65e6ca877f6006cba1459c9
Roykristanto88 <61072838+Roykristanto88@users.noreply.github.com>
2020-03-21 00:01:41 +0700

    Update Home.php

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,17 +2,17 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends CI_Controller {
-	function index() {
+    function index() {
+        //memanggil HomeModel di folder models
+        $this->load->model('HomeModel'); 
+        
+        //menampung fungsi dari getData()
+        $tampilkanData = $this->HomeModel->getData();
 
-$tampilkanData=$this->HomeModel->getData();
+        $this->load->view('home', array("tampilkanData" => $tampilkanData));
 
-		$this->load->view('home',array("tampilkanData" => $tampilkanData));
-		//$this->load->model('HomeModel');
-		//menampung fungsi dari getData()
-		//$tampilkanData= $this->HomeModel->getData();
-
-		//echo "Nama:". $tampilkanData['Nama']. "<br>";
-		//echo "kampus:". $tampilkanData['Kampus']. "<br>";
-		//echo "website:". $tampilkanData['Website']. "<br>";
-}
+        echo "Nama: " . $tampilkanData['Nama'] . "<br>";
+        echo "Kampus: " . $tampilkanData['Kampus'] . "<br>";
+        echo "Website: " . $tampilkanData['Website'] . "<br>";    
+    }
 }
